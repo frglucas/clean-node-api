f616975af64732c6e79fb5e371b256ded862ba6e
Lucas Da Silva Fraga <lucas.fraga@cwi.com.br>
2023-07-21 20:59:31 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
+  coverageProvider: 'babel',
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
